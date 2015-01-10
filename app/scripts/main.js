720ea377d88098bd773e2bb408a6e8632b22f47a
pramodsvidyarthi <pramodsvidyarthi@yahoo.com>
2015-01-10 16:43:36 +0530

    added shims and paths

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('\'Allo \'Allo!');
+require.config({
+	paths:{
+		'jquery':'../../bower_components/jquery/dist/jquery',
+		'backbone':'../../bower_components/backbone/backbone',
+		'underscore':'../../bower_components/underscore/underscore'
+	},
+	shim:{
+		'backbone':{
+			'deps':['jquery','underscore'],
+			'exports':'Backbone'
+		}
+	}
+});
+
+require(['backbone'],function (bb) {
+	console.log(bb);
+});
